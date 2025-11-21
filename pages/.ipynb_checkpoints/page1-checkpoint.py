a13b9129e030abde8f71d51481e7d7f1454d4b9f
Athaya <athayatalitasyakira@gmail.com>
2025-11-21 19:16:16 +0700

    menambahkan file page1, page2, page3

---
+++ pages/.ipynb_checkpoints/page1-checkpoint.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("welcome to athaya's homepage")
+st.write("ini demo aplikasi streamlit.")
---
+++ pages/page3.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Settings")
+st.write("This Page Allows You To Customize Your Preferences")
